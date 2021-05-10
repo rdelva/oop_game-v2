84c7c0d624af5ffe4fe242ce452528a6539c1bc9
rdelva <roseldelva@gmail.com>
2021-05-10 18:45:18 -0400

    add array and phrases and assign it to property phrases

--- js/Game.js
+++ js/Game.js
@@ -4,7 +4,25 @@
 class Game {
     constructor(){
         this.missed = 0;
-        this.phrases = [];
+        this.phrases = phrases;
         this.activePhrase = null;
     }
+
+    /**
+* Creates phrases for use in game
+* @return {array} An array of phrases that could be used in the game
+*/
+
+    createPhrase(){
+        const phrases = [
+            'Jack of All Trades, Master of None',
+            'Don’t Count Your Chickens Before They Hatch',
+            'I\'m gonna make him an offer he can\'t refuse.',
+            'Bond. James Bond',
+            'Show me the money!'
+        ]
+
+        return phrases;
+    }
+
 }
