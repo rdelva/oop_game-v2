0f6c2b2af675aaae502fcca399565bebff836c0f
rdelva <roseldelva@gmail.com>
2021-05-19 23:02:50 -0400

    update line 14 to output from Game.js

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ const game = new Game();
 //const phrase = new Phrase('Life is like a box of chocolates');
 //console.log(`Phrase - phrase: ${phrase.phrase}`);
 
+
 game.phrases.forEach((phrase, index) => {
-    console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
-});
+  
+    console.log(`Phrase ${index} - phrase: ${phrase}`);
+})
