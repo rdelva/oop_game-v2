b889f235c908fd7188184d10edf0a9b6fa3840bd
rdelva <roseldelva@gmail.com>
2021-05-28 22:08:07 -0400

    added createPhrase()

--- js/Game.js
+++ js/Game.js
@@ -5,7 +5,24 @@
 class Game {
     constructor(){
         this.missed = 0;
-        this.phrases = [];
+        this.phrases = this.createPhrases();
         this.activePhrase = null;
     }
+
+    /**
+    * Creates phrases for use in game
+    * @return {array} An array of phrases that could be used in the game
+    */
+    createPhrases() {
+        const phraseSet = [
+            new Phrase('THIS IS SPARTA'),
+            new Phrase('Nobody puts Baby in a corner'),
+            new Phrase('My precious'),
+            new Phrase('Rosebud'),
+            new Phrase('Inconceivable'),
+        ]
+        return phraseSet;
+    }
+
 }
+
