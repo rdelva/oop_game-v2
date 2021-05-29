da4738e8913fb5c387b1422d29c71f943b31c7af
rdelva <roseldelva@gmail.com>
2021-05-28 22:18:32 -0400

    added then removed code for testing

--- js/app.js
+++ js/app.js
@@ -4,10 +4,22 @@
 
 
 // const phrase = new Phrase();
-// const game = new Game();
+//  const game = new Game();
+//  game.getRandomPhrase();
+
 // const phrase = new Phrase('Life is like a box of chocolates');
 // console.log(`Phrase - phrase: ${phrase.phrase}`);
 // const game = new Game();
 // game.phrases.forEach((phrase, index) => {
 // console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
 // });
+
+// const logPhrase = (phrase) => {
+//     console.log(`Phrase - phrase: `, phrase.phrase);
+//     };
+//     const game = new Game();
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
