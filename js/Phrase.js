7f5003d100910683d435de0640c97669159423e3
rdelva <roseldelva@gmail.com>
2021-05-28 23:30:01 -0400

    add in addPhraseToDisplay()

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,4 +6,22 @@ class Phrase {
     constructor(phrase){
         this.phrase = phrase.toLowerCase();
     }
-}
+
+    /**
+    * Display phrase on game board
+    */
+    addPhraseToDisplay() {
+        const phraseSection = document.getElementById("phrase");
+        const ul = phraseSection.querySelector("ul");        
+    
+        for(let i  = 0; i < this.phrase.length; i++){
+          
+            if(this.phrase[i] !== " "){
+                ul.innerHTML += `<li class="hide letter">${this.phrase[i]}</li>`;
+            } else {
+                ul.innerHTML += `<li class="space">${this.phrase[i]}</li>`;
+            }
+        }
+    }
+
+}// end of Phrase class
