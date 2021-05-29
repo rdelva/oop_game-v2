911519a030f8528930f052028a2b2aa89446dccf
rdelva <roseldelva@gmail.com>
2021-05-29 00:34:32 -0400

    add in addEventListener to the start button to start the game

--- js/app.js
+++ js/app.js
@@ -3,23 +3,10 @@
  * app.js */
 
 
-// const phrase = new Phrase();
-//  const game = new Game();
-//  game.getRandomPhrase();
 
-// const phrase = new Phrase('Life is like a box of chocolates');
-// console.log(`Phrase - phrase: ${phrase.phrase}`);
-// const game = new Game();
-// game.phrases.forEach((phrase, index) => {
-// console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
-// });
+const startButton = document.getElementById("btn__reset");
 
-// const logPhrase = (phrase) => {
-//     console.log(`Phrase - phrase: `, phrase.phrase);
-//     };
-//     const game = new Game();
-//     logPhrase(game.getRandomPhrase());
-//     logPhrase(game.getRandomPhrase());
-//     logPhrase(game.getRandomPhrase());
-//     logPhrase(game.getRandomPhrase());
-//     logPhrase(game.getRandomPhrase());
+startButton.addEventListener('click', (e) => {
+    const game = new Game();
+    game.startGame(); 
+});
