6f893538cf5dcf676a60345c1f20daf66b901bbc
rdelva <roseldelva@gmail.com>
2021-05-28 21:28:26 -0400

    deleted everything starting over to understand better

--- js/Phrase.js
+++ js/Phrase.js
@@ -2,30 +2,4 @@
  * Project 4 - OOP Game App
  * Phrase.js */
 
-class Phrase {
-    constructor(phrase){
-      this.phrase = phrase.toLowerCase();       
-    }
-
-
-/**
-* Display phrase on game board
-*/
-addPhraseToDisplay() {
-    const phraseSection = document.getElementById("phrase");
-    const ul = phraseSection.querySelector("ul");
-
-    console.log(this.phrase)
-    for(let i = 0; i < this.phrase.length; i++) {
-             if(this.phrase[i] !== ' '){
-            ul.innerHTML += `<li class="hide letter">${this.phrase[i]}</li>`;
-        } else {
-            ul.innerHTML += `<li class="space">${this.phrase[i]}</li>`
-        }
-
-    }
-
-}
-
-} 
 
--- js/app.js
+++ js/app.js
@@ -3,20 +3,3 @@
  * app.js */
 
 
-const game = new Game();
-
-
-/*game.phrases.forEach((phrase, index) => {
-console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
-});
-
-const logPhrase = (phrase) => {
-    console.log(`Phrase - phrase: `, phrase.phrase);
-    };
- 
-    logPhrase(game.getRandomPhrase());
-   
-*/
-//game.getRandomPhrase().addPhraseToDisplay();
-game.startGame();
-//console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
