9f0a514433aea7d72a2681f314c386c59ddb89ee
rdelva <roseldelva@gmail.com>
2021-05-31 21:36:14 -0400

    changed event handler to keep it simple

--- js/app.js
+++ js/app.js
@@ -4,36 +4,14 @@
 
 
 
-// Helper Function
-const start = (e) => {
-   
-    if(e.target.id == 'BUTTON'){
+// if you select the Start Button the  game will begin
+const startButton = document.getElementById("btn__reset");
+window.addEventListener('click', (e) => {
+    if(e.target.id === startButton.id){
         console.log("HI");
         const game = new Game();
         game.startGame();
         game.handleInteraction();
         game.activePhrase.checkLetter('a');
     } 
-}
-
-window.addEventListener('click', start);
-
-
-// if you select the keyboard on screen
-// const keyboard = document.getElementById("qwerty");
-
-// keyboard.addEventListener('click', (e) => {
-//     if(e.target.tagName == 'BUTTON' ){
-//         const chosenLetter = e.target.innerHTML;
-//         e.target.classList.add("chosen");             
-//     }
-// }); 
-
-/*
-//if you use your own keyboard
-window.addEventListener('keyup', (e) => {
-    console.log();
-    if(e.key == 'Enter' || e.key == 'Spacebar'){
-
-    }
-});*/
+});
