d896a67abce2b5f0bc3be08cdfd369fc3b7a5ed2
rdelva <roseldelva@gmail.com>
2021-05-31 22:50:32 -0400

    moved game object out of event listener so it can be refered by other functions.  updated event listner for qwerty

--- js/app.js
+++ js/app.js
@@ -3,15 +3,32 @@
  * app.js */
 
 
+const game = new Game();
 
 // if you select the Start Button the  game will begin
 const startButton = document.getElementById("btn__reset");
 window.addEventListener('click', (e) => {
-    if(e.target.id === startButton.id){
-        console.log("HI");
-        const game = new Game();
+    if(e.target.id === startButton.id){      
         game.startGame();
         game.handleInteraction();
-        game.activePhrase.checkLetter('a');
+        //game.activePhrase.checkLetter('a');
     } 
+});
+
+
+
+// Selects the entire screen keyboard
+const qwerty  = document.getElementById("qwerty");
+
+
+qwerty.addEventListener('click', (e) => {
+    //chosenLetter = e.target; 
+
+    if(e.target.tagName === 'BUTTON'){
+        console.log(e.target.innerHTML);
+        e.target.classList.add('chosen');
+        game.handleInteraction(e.target.innerHTML);
+    }
+
+
 });
